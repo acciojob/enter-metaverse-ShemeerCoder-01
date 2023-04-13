47a7b011e81141b22485691d3eea3c68ea3ea062
ShemeerCoder-01 <118423467+ShemeerCoder-01@users.noreply.github.com>
2023-04-13 15:20:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+function changeData(){
+	document.getElementById("status").style.display = "none";
+	document.getElementById("big").innerText = "Entered the Metaverse";
+	
+}
