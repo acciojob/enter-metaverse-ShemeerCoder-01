dff8cd73222658eaf03965614a004ee00ab2bc94
ShemeerCoder-01 <118423467+ShemeerCoder-01@users.noreply.github.com>
2023-04-13 15:38:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 function changeData(){
 	let tag = document.getElementById("status");
-	tag.innerHTML = `<h1>Entered the Metaverse</h1>`;
+	tag.innerHTML = `<h1>Entered Metaverse</h1>`;
 	
 }
